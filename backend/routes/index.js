3ca0fb9ebffbaca84a220ad4f1aa50fb1efae6c2
Art Furrer <2823246+bonham@users.noreply.github.com>
2020-09-20 21:38:35 +0200

    backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
-router.get('/name', function(req, res, next) {
-  res.json({'name':'Michael'})
+router.get('/name', function (req, res, next) {
+  let randInt = Math.floor(Math.random() * 11)
+  let name = "Michael " + randInt
+  res.json({ 'name': name })
 });
 
 module.exports = router;
