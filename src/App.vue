51c3d9392118052a3eb0342b3717504719a2e3fa
Art Furrer <2823246+bonham@users.noreply.github.com>
2020-09-22 23:36:17 +0200

    scss 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <custom-nav-bar/>
-    <hello-world msg="Welcome to Your Vue.js App"/>
-    <nav-bar-scrollable/>
+    <custom-nav-bar />
+    <hello-world msg="Welcome to Your Vue.js App" />
+    <nav-bar-scrollable />
     <b-container>
       <b-row>
         <b-col>Hallo</b-col>
@@ -10,36 +10,35 @@
         <b-col>Hallo</b-col>
       </b-row>
     </b-container>
-
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import { BContainer, BRow, BCol } from 'bootstrap-vue'
-import CustomNavBar from './components/CustomNavBar.vue'
-import NavBarScrollable from './components/NavBarScrollable.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { BContainer, BRow, BCol } from "bootstrap-vue";
+import CustomNavBar from "./components/CustomNavBar.vue";
+import NavBarScrollable from "./components/NavBarScrollable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     BContainer,
     BRow,
     BCol,
     CustomNavBar,
-    NavBarScrollable
-  }
-}
+    NavBarScrollable,
+  },
+};
 </script>
 
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+<style lang="scss">
+$body-bg: red;
+// Import custom SASS variable overrides, or alternatively
+// define your variable overrides here instead
+@import "assets/custom-vars.scss";
+
+// Import Bootstrap and BootstrapVue source SCSS files
+@import "~bootstrap/scss/bootstrap.scss";
+@import "~bootstrap-vue/src/index.scss";
 </style>
